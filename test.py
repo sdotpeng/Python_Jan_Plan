85141d9b2b28f38ab3ffbac1ed5f22d0f3c878fe
Siyuan PENG <sn0wbar@qq.com>
2021-01-17 22:23:22 -0500

    Updated

---
+++ test.py
@@ -0,0 +1,19 @@
+def f(var1, var2):
+    print('var1:', var1)
+    print('var2:', var2)
+
+# f(var1=3, var2=4)
+# f(var2=4, var1=3)
+# f(var1=3, var1=3)
+# f(var1=3, var3=1000)
+
+for i in range(10):
+    output = i + 2
+
+print(output)
+print(i)
+
+for i in range(5):
+    print(i)
+
+print(bool(10) + (10 // 3 == 3) == 4)
