fca644760ea797b01bd7a6ece2dd14225b6579b6
Siyuan PENG <sn0wbar@qq.com>
2021-01-26 11:29:33 -0500

    Updated Jan_26

---
+++ Jan_26/Image.py
@@ -0,0 +1,16 @@
+# Images
+# For the next weeks, we'll focus on an important area of computer science called image processing
+
+''' What is an image? '''
+''' You are probably accustomed to working with .jpg, .gif, .png, and
+other types of image on your computer'''
+
+''' How do computers store the data that make up an image? It's represented as a 2D
+regular grid of pixel values'''
+'''We'll say that there are x columns and y rows'''
+
+'''Recall that Python 'counts' starting at 0 and goes to n-1. So the pixel numbering in
+the grid go from (0,0) on the top-left corner to (X-1, Y-1) on the bottom right corner,
+where again X is the image width in pixels and Y is the image height in pixels (X columns =
+width, Y rows = height). This seemingly strangle coordinate system is common to images and
+computer graphics'''
